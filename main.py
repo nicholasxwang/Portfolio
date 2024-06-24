513e174f490293d594ac537442e43fe2bcaea3e5
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2024-06-24 15:13:34 -0400

    fixed a lot of ui

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
-@app.route("/projects")
-def projects():
-    return render_template("projects.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
